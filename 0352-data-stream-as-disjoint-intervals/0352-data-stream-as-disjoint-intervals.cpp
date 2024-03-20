20e9c2eeeec8c5c170f973adcb06759383a095a0
udaynayar <120821755+udaynayar@users.noreply.github.com>
2024-03-20 22:06:43 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (66.96%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -0,0 +1,27 @@
+class SummaryRanges {
+ 
+ 
+public:
+    set<int>st;
+    SummaryRanges() {}
+    void addNum(int value) { st.insert(value); }
+    vector<vector<int>> getIntervals() {
+        if (st.empty()) {
+            return {};
+        }
+        vector<vector<int>> ans;
+        int start = -1, last = -1;
+        for (int val : st) {
+            if (start < 0) {
+                start = last = val;
+            } else if (val-last==1) {
+                last = val;
+            } else {
+                ans.push_back({start, last});
+                start = last = val;
+            }
+        }
+        ans.push_back({start, last});
+        return ans;
+    }
+};
