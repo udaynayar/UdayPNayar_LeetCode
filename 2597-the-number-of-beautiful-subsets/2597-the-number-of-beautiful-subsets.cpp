de277181fd133e08e13b3727cb0088e7487a7940
udaynayar <120821755+udaynayar@users.noreply.github.com>
2024-05-23 14:23:35 +0530

    Time: 121 ms (68.39%), Space: 36.9 MB (79.75%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    char freq[1001] = {0};
-    int cnt = 0, n;
-    void backtrack(int idx, vector<int>& nums, int k) {
-        if (idx==n) {
-            cnt++;
+    
+    void recurse(int freq_dp[], vector<int>& nums, int k, int& ans, int i, int n) {
+        
+        if (i == n) {       
+            ans++;
             return;
         }
-        backtrack(idx+1, nums, k); // skip current number
-        int x = nums[idx];
-        if ((x - k < 0 || freq[x - k] == 0) &&
-            (x + k > 1000 || freq[x + k] == 0)) {
-            freq[x]++;
-            backtrack(idx+1, nums, k); // include current number
-            freq[x]--;
+
+        recurse(freq_dp, nums, k, ans, i+1, n);
+        
+        if (nums[i]-k < 0 || (freq_dp[nums[i] - k] == 0) && (nums[i]+k > 1000 || (freq_dp[nums[i] + k] == 0))) {
+            
+            freq_dp[nums[i]]++;
+            recurse(freq_dp, nums, k, ans, i+1, n);
+            freq_dp[nums[i]]--;
+            
         }
+        
+        
     }
+    
+    
     int beautifulSubsets(vector<int>& nums, int k) {
-        n = nums.size();
+        
+        int freq_dp[1001] = {0};
+        int ans = 0;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        backtrack(0, nums, k);
-        return --cnt; // No empty subset
+        recurse(freq_dp, nums, k, ans, 0, n);
+        
+        
+        
+        return ans - 1;
     }
 };
