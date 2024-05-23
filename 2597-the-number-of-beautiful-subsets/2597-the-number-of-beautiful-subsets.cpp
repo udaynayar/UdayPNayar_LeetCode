e9ee8fb653e9c853d5de83af51d18f0f0a4cf817
udaynayar <120821755+udaynayar@users.noreply.github.com>
2024-05-23 14:25:20 +0530

    Time: 118 ms (69.01%), Space: 36.7 MB (80.99%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -3,14 +3,15 @@ public:
     
     void recurse(int freq_dp[], vector<int>& nums, int k, int& ans, int i, int n) {
         
-        if (i == n) {       
+        if (i == n) {
+            
             ans++;
             return;
         }
 
         recurse(freq_dp, nums, k, ans, i+1, n);
         
-        if (nums[i]-k < 0 || (freq_dp[nums[i] - k] == 0) && (nums[i]+k > 1000 || (freq_dp[nums[i] + k] == 0))) {
+        if ((nums[i]+k > 1000 || (freq_dp[nums[i] + k] == 0)) && (nums[i]-k < 0 || (freq_dp[nums[i] - k] == 0))) {
             
             freq_dp[nums[i]]++;
             recurse(freq_dp, nums, k, ans, i+1, n);
